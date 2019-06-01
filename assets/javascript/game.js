dcb7518acbf0cc55638f6d25147772366aa46dfb
lara bergerson <lara.bergerson@gmail.com>
2019-06-01 17:34:19 -0500

    added some js and basic css that's not working.

---
+++ assets/javascript/game.js
@@ -0,0 +1,37 @@
+var welcome = "Welcome to the game!"
+// var ready = true
+
+alert("are you ready to play?");
+
+var gotCharacters = [
+    "Jon",
+    "Arya", 
+    "Sansa", 
+    "Tyrion", 
+    "Cersei", 
+    "Dany", 
+    "Drogon"]
+var numGuesses = 10
+var outcome = true
+
+var guess = gotCharacters[Math.floor(math.random() * gotCharacters.length)];    
+
+
+var answers = [];
+for (var i=0; i < guess.length; i++){
+    ansewrs[i] = "_";
+}
+
+var remainingLetters = word.length;
+
+document.onkeyup = function(event) {
+
+    var userGuess = event.key;
+
+
+var userPicks = prompt("Guess a letter.");
+
+}
+
+
+
